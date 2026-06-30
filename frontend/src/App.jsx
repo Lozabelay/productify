1585dc1d7e639866d4a30cf6639222bf331dc924
Loza Belay <finotbelay88@gmail.com>
2026-06-30 14:29:38 +0300

    feat: integrate DaisyUI for enhanced styling and theming

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ function App() {
     <div className="">
       <h1 className="text-blue-500"> welcome to productify</h1>
 
+      <button className=" btn btn-primary" > Click me</button>
+
     </div>
   )
   
