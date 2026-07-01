7d53b2ec6afdf786099b08fffc26cd0c5f9d6306
Loza Belay <finotbelay88@gmail.com>
2026-07-02 00:21:43 +0300

    feat: update page components to reflect correct names and structure for Home, Product, Profile, Create, and Edit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,8 +13,8 @@ import ProductPage from "./pages/ProductPage";
 import ProfilePage from "./pages/ProfilePage";
 import CreatePage from "./pages/CreatePage";
 import EditProductPage from "./pages/EditProductPage";
-import useAuthReq from "./hooks/useAuthReq";
-import useUserSync from "./hooks/useUserSync";
+//import useAuthReq from "./hooks/useAuthReq";
+//import useUserSync from "./hooks/useUserSync";
 
 function App() {
   return (
@@ -24,12 +24,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/profile" element={isSignedIn ? <ProfilePage /> : <Navigate to={"/"} />} />
-          <Route path="/create" element={isSignedIn ? <CreatePage /> : <Navigate to={"/"} />} />
-          <Route
-            path="/edit/:id"
-            element={isSignedIn ? <EditProductPage /> : <Navigate to={"/"} />}
-          />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/edit/:id" element={<EditProductPage />} />
         </Routes>
       </main>
     </div>
