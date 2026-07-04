998355df83ad79612d105ab16a4ab29a7c3ff633
Loza Belay <finotbelay88@gmail.com>
2026-07-05 00:18:01 +0300

    feat: refactor environment configuration, enhance API integration, and implement user synchronization hooks

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
+import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
-import {QueryClient,QueryClientProvider,
-} from '@tanstack/react-query'
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Add your Clerk Publishable Key to the .env file");
+  throw new Error("Missing Publishable Key");
 }
-     // Create a client
-const queryClient = new QueryClient()
 
+// Create a client
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
@@ -24,5 +25,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
