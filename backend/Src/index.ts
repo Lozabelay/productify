f1c4fcfc1c8c57239597bb83dedb563cc8182064
Loza Belay <finotbelay88@gmail.com>
2026-07-08 14:46:29 +0300

    added feature

--- backend/Src/index.ts
+++ backend/Src/index.ts
@@ -9,18 +9,30 @@ import userRoutes from "./routes/userRoutes";
 import productRoutes from "./routes/productRoutes";
 import commentRoutes from "./routes/commentRoutes";
 
+// Create express app
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:5173', // Do not include other header names here
-  credentials: true                // This sets 'Access-Control-Allow-Credentials' separately
-}));
-app.use(clerkMiddleware()); // auth obj will be attached to the req
-app.use(express.json()); // parses JSON request bodies.
-app.use(express.urlencoded({ extended: true })); // parses form data (like HTML forms).
 
+// CORS configuration
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://productify-1-i9lq.onrender.com",
+    ],
+    credentials: true,
+  })
+);
+
+// Middleware
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Health check
 app.get("/api/health", (req, res) => {
   res.json({
-    message: "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+    message:
+      "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
     endpoints: {
       users: "/api/users",
       products: "/api/products",
@@ -29,20 +41,31 @@ app.get("/api/health", (req, res) => {
   });
 });
 
+// API routes
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/comments", commentRoutes);
 
+
+// Serve frontend in production
 if (ENV.NODE_ENV === "production") {
   const __dirname = path.resolve();
 
-  // serve static files from frontend/dist
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // handle SPA routing - send all non-API routes to index.html - react app
+  // React Router fallback
   app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    );
   });
 }
 
-app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+
+// Start server
+app.listen(ENV.PORT, () => {
+  console.log(
+    "Server is up and running on PORT:",
+    ENV.PORT
+  );
+});
